b28a896167d7c353ed2838d781ba0a51d8d761ba
vroopaniaj08 <apoorvjain7222@gmail.com>
2024-01-02 14:14:17 +0530

    pattern

---
+++ pattern_02.java
@@ -0,0 +1,17 @@
+package pattern.patterns;
+import java.util.Scanner;
+public class pattern_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=0;i<n;i++){
+            for (int j=n-1;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
